18f72c27bac9b9bd7b0d8672c9aca9586a2fdece
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-12-12 11:13:15 +0700

    Example of topdown camera

---
+++ Scripts/TopDownPlayerCharacterController.cs
@@ -0,0 +1,271 @@
+﻿using LiteNetLibManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class TopDownPlayerCharacterController : BasePlayerCharacterController
+    {
+        public enum PlayerCharacterControllerMode
+        {
+            PointClick,
+            WASD,
+            Both,
+        }
+
+        public struct UsingSkillData
+        {
+            public Vector3? position;
+            public int dataId;
+            public UsingSkillData(Vector3? position, int dataId)
+            {
+                this.position = position;
+                this.dataId = dataId;
+            }
+        }
+
+        public const float DETECT_MOUSE_DRAG_DISTANCE = 10f;
+        public const float DETECT_MOUSE_HOLD_DURATION = 1f;
+        public DimensionType dimensionType;
+        public PlayerCharacterControllerMode controllerMode;
+        [Tooltip("Set this to TRUE to find nearby enemy and look to it while attacking when `Controller Mode` is `WASD`")]
+        public bool wasdLockAttackTarget;
+        [Tooltip("This will be used to find nearby enemy when `Controller Mode` is `Point Click` or when `Wasd Lock Attack Target` is `TRUE`")]
+        public float lockAttackTargetDistance = 10f;
+        public FollowCameraControls gameplayCameraPrefab;
+        public GameObject targetObjectPrefab;
+        [Header("Building Settings")]
+        public bool buildGridSnap;
+        public float buildGridSize = 4f;
+        public bool buildRotationSnap;
+
+        protected Vector3? destination;
+        protected UsingSkillData? queueUsingSkill;
+        protected Vector3 mouseDownPosition;
+        protected float mouseDownTime;
+        protected bool isMouseDragOrHoldOrOverUI;
+        protected uint lastNpcObjectId;
+
+        public FollowCameraControls CacheGameplayCameraControls { get; protected set; }
+        public GameObject CacheTargetObject { get; protected set; }
+
+        protected BaseGameEntity targetEntity;
+        protected Vector3 targetPosition;
+        // Optimizing garbage collection
+        protected bool getMouseUp;
+        protected bool getMouseDown;
+        protected bool getMouse;
+        protected bool isPointerOverUI;
+        protected bool isMouseDragDetected;
+        protected bool isMouseHoldDetected;
+        protected bool isMouseHoldAndNotDrag;
+        protected BaseCharacterEntity targetCharacter;
+        protected BaseCharacterEntity targetEnemy;
+        protected BasePlayerCharacterEntity targetPlayer;
+        protected BaseMonsterCharacterEntity targetMonster;
+        protected NpcEntity targetNpc;
+        protected ItemDropEntity targetItemDrop;
+        protected BuildingEntity targetBuilding;
+        protected HarvestableEntity targetHarvestable;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            buildingItemIndex = -1;
+            currentBuildingEntity = null;
+
+            if (gameplayCameraPrefab != null)
+            {
+                // Set parent transform to root for the best performance
+                CacheGameplayCameraControls = Instantiate(gameplayCameraPrefab);
+            }
+            if (targetObjectPrefab != null)
+            {
+                // Set parent transform to root for the best performance
+                CacheTargetObject = Instantiate(targetObjectPrefab);
+                CacheTargetObject.SetActive(false);
+            }
+        }
+
+        protected override void Setup(BasePlayerCharacterEntity characterEntity)
+        {
+            base.Setup(characterEntity);
+
+            if (characterEntity == null)
+                return;
+
+            if (CacheGameplayCameraControls != null)
+                CacheGameplayCameraControls.target = characterEntity.CacheTransform;
+        }
+
+        protected override void Desetup(BasePlayerCharacterEntity characterEntity)
+        {
+            base.Desetup(characterEntity);
+
+            if (CacheGameplayCameraControls != null)
+                CacheGameplayCameraControls.target = null;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (CacheGameplayCameraControls != null)
+                Destroy(CacheGameplayCameraControls.gameObject);
+            if (CacheTargetObject != null)
+                Destroy(CacheTargetObject.gameObject);
+        }
+
+        protected override void Update()
+        {
+            if (PlayerCharacterEntity == null || !PlayerCharacterEntity.IsOwnerClient)
+                return;
+
+            base.Update();
+
+            if (CacheTargetObject != null)
+                CacheTargetObject.gameObject.SetActive(destination.HasValue);
+
+            if (PlayerCharacterEntity.IsDead())
+            {
+                queueUsingSkill = null;
+                destination = null;
+                if (CacheUISceneGameplay != null)
+                    CacheUISceneGameplay.SetTargetCharacter(null);
+                CancelBuild();
+            }
+            else
+            {
+                targetCharacter = null;
+                PlayerCharacterEntity.TryGetTargetEntity(out targetCharacter);
+                if (CacheUISceneGameplay != null)
+                    CacheUISceneGameplay.SetTargetCharacter(targetCharacter);
+            }
+
+            if (destination.HasValue)
+            {
+                if (CacheTargetObject != null)
+                    CacheTargetObject.transform.position = destination.Value;
+                if (Vector3.Distance(destination.Value, CharacterTransform.position) < StoppingDistance + 0.5f)
+                    destination = null;
+            }
+
+            UpdateInput();
+            UpdateFollowTarget();
+        }
+
+        private Vector3 GetBuildingPlacePosition(Vector3 position)
+        {
+            if (buildGridSnap)
+                position = new Vector3(Mathf.Round(position.x / buildGridSize) * buildGridSize, position.y, Mathf.Round(position.z / buildGridSize) * buildGridSize);
+            return position;
+        }
+
+        private Vector3 GetBuildingPlaceEulerAngles(Vector3 eulerAngles)
+        {
+            eulerAngles.x = 0;
+            eulerAngles.z = 0;
+            // Make Y rotation set to 0, 90, 180
+            if (buildRotationSnap)
+                eulerAngles.y = Mathf.Round(eulerAngles.y / 90) * 90;
+            return eulerAngles;
+        }
+
+        public bool TryGetAttackingCharacter(out BaseCharacterEntity character)
+        {
+            character = null;
+            if (PlayerCharacterEntity.TryGetTargetEntity(out character))
+            {
+                if (character.CanReceiveDamageFrom(PlayerCharacterEntity))
+                    return true;
+                else
+                    character = null;
+            }
+            return false;
+        }
+
+        public bool GetAttackDistanceAndFov(out float attackDistance, out float attackFov)
+        {
+            attackDistance = PlayerCharacterEntity.GetAttackDistance();
+            attackFov = PlayerCharacterEntity.GetAttackFov();
+            if (queueUsingSkill.HasValue)
+            {
+                var queueUsingSkillValue = queueUsingSkill.Value;
+                Skill skill = null;
+                if (GameInstance.Skills.TryGetValue(queueUsingSkillValue.dataId, out skill) && skill != null)
+                {
+                    if (skill.IsAttack())
+                    {
+                        attackDistance = PlayerCharacterEntity.GetSkillAttackDistance(skill);
+                        attackFov = PlayerCharacterEntity.GetSkillAttackFov(skill);
+                    }
+                    else
+                    {
+                        // Stop movement to use non attack skill
+                        PlayerCharacterEntity.StopMove();
+                        RequestUsePendingSkill();
+                        return false;
+                    }
+                }
+                else
+                    queueUsingSkill = null;
+            }
+            return true;
+        }
+
+        public bool IsLockTarget()
+        {
+            return controllerMode == PlayerCharacterControllerMode.Both ||
+                controllerMode == PlayerCharacterControllerMode.PointClick ||
+                (controllerMode == PlayerCharacterControllerMode.WASD && wasdLockAttackTarget);
+        }
+
+        public Vector3 GetMoveDirection(float horizontalInput, float verticalInput)
+        {
+            var moveDirection = Vector3.zero;
+            switch (dimensionType)
+            {
+                case DimensionType.Dimension3D:
+                    moveDirection += Camera.main.transform.forward * verticalInput;
+                    moveDirection += Camera.main.transform.right * horizontalInput;
+                    moveDirection.y = 0;
+                    break;
+                case DimensionType.Dimension2D:
+                    moveDirection = new Vector2(horizontalInput, verticalInput);
+                    break;
+            }
+            return moveDirection;
+        }
+
+        public void RequestAttack()
+        {
+            PlayerCharacterEntity.RequestAttack();
+        }
+
+        public void RequestUseSkill(Vector3 position, int dataId)
+        {
+            PlayerCharacterEntity.RequestUseSkill(position, dataId);
+        }
+
+        public void RequestUsePendingSkill()
+        {
+            if (queueUsingSkill.HasValue)
+            {
+                var queueUsingSkillValue = queueUsingSkill.Value;
+                var position = queueUsingSkillValue.position.HasValue ? queueUsingSkillValue.position.Value : CharacterTransform.position;
+                RequestUseSkill(position, queueUsingSkillValue.dataId);
+                queueUsingSkill = null;
+            }
+        }
+
+        public void RequestEquipItem(ushort itemIndex)
+        {
+            PlayerCharacterEntity.RequestEquipItem(itemIndex);
+        }
+
+        public void RequestUseItem(ushort itemIndex)
+        {
+            PlayerCharacterEntity.RequestUseItem(itemIndex);
+        }
+    }
+}
---
+++ Scripts/TopDownPlayerCharacterController_FindObjects.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class TopDownPlayerCharacterController
+    {
+        public const int RAYCAST_COLLIDER_SIZE = 32;
+        public const int OVERLAP_COLLIDER_SIZE = 32;
+        protected RaycastHit[] raycasts = new RaycastHit[RAYCAST_COLLIDER_SIZE];
+        protected Collider[] overlapColliders = new Collider[OVERLAP_COLLIDER_SIZE];
+        protected RaycastHit2D[] raycasts2D = new RaycastHit2D[RAYCAST_COLLIDER_SIZE];
+        protected Collider2D[] overlapColliders2D = new Collider2D[OVERLAP_COLLIDER_SIZE];
+        protected GameObject tempGameObject;
+        protected Transform tempTransform;
+        protected Vector3 tempVector3;
+        protected int tempCount;
+        protected int tempCounter;
+
+        public int FindClickObjects(out Vector3 worldPointFor2D)
+        {
+            worldPointFor2D = Vector3.zero;
+            if (dimensionType == DimensionType.Dimension3D)
+                return Physics.RaycastNonAlloc(Camera.main.ScreenPointToRay(Input.mousePosition), raycasts, 100f, gameInstance.GetTargetLayerMask());
+            worldPointFor2D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return Physics2D.LinecastNonAlloc(worldPointFor2D, worldPointFor2D, raycasts2D, gameInstance.GetTargetLayerMask());
+        }
+
+        public void FindAndSetBuildingAreaFromMousePosition()
+        {
+            tempCount = 0;
+            switch (dimensionType)
+            {
+                case DimensionType.Dimension3D:
+                    tempCount = Physics.RaycastNonAlloc(Camera.main.ScreenPointToRay(Input.mousePosition), raycasts, 100f, gameInstance.GetBuildLayerMask());
+                    break;
+                case DimensionType.Dimension2D:
+                    tempVector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    tempCount = Physics2D.LinecastNonAlloc(tempVector3, tempVector3, raycasts2D, gameInstance.GetBuildLayerMask());
+                    break;
+            }
+            LoopSetBuildingArea(tempCount);
+        }
+
+        public void FindAndSetBuildingAreaFromCharacterDirection()
+        {
+            if (currentBuildingEntity == null)
+                return;
+            tempCount = 0;
+            switch (dimensionType)
+            {
+                case DimensionType.Dimension3D:
+                    tempVector3 = CharacterTransform.position + (CharacterTransform.forward * currentBuildingEntity.characterForwardDistance);
+                    currentBuildingEntity.CacheTransform.eulerAngles = GetBuildingPlaceEulerAngles(CharacterTransform.eulerAngles);
+                    currentBuildingEntity.buildingArea = null;
+                    tempCount = Physics.RaycastNonAlloc(new Ray(tempVector3 + (Vector3.up * 2.5f), Vector3.down), raycasts, 5f, gameInstance.GetBuildLayerMask());
+                    break;
+                case DimensionType.Dimension2D:
+                    // TODO: implement this
+                    break;
+            }
+
+            if (!LoopSetBuildingArea(tempCount))
+                currentBuildingEntity.CacheTransform.position = GetBuildingPlacePosition(tempVector3);
+        }
+
+        private bool LoopSetBuildingArea(int count)
+        {
+            BuildingArea nonSnapBuildingArea = null;
+            for (tempCounter = 0; tempCounter < count; ++tempCounter)
+            {
+                tempTransform = GetRaycastTransform(tempCounter);
+                tempVector3 = GetRaycastPoint(tempCounter);
+                if (Vector3.Distance(tempVector3, CharacterTransform.position) > gameInstance.buildDistance)
+                    return false;
+
+                var buildingArea = tempTransform.GetComponent<BuildingArea>();
+                if (buildingArea == null || (buildingArea.buildingEntity != null && buildingArea.buildingEntity == currentBuildingEntity))
+                    continue;
+
+                if (currentBuildingEntity.buildingType.Equals(buildingArea.buildingType))
+                {
+                    currentBuildingEntity.CacheTransform.position = GetBuildingPlacePosition(tempVector3);
+                    currentBuildingEntity.buildingArea = buildingArea;
+                    if (buildingArea.snapBuildingObject)
+                        return true;
+                    nonSnapBuildingArea = buildingArea;
+                }
+            }
+            if (nonSnapBuildingArea != null)
+                return true;
+            return false;
+        }
+
+        public Transform GetRaycastTransform(int index)
+        {
+            if (dimensionType == DimensionType.Dimension3D)
+                return raycasts[index].transform;
+            return raycasts2D[index].transform;
+        }
+
+        public Vector3 GetRaycastPoint(int index)
+        {
+            if (dimensionType == DimensionType.Dimension3D)
+                return raycasts[index].point;
+            return raycasts2D[index].point;
+        }
+
+        public int OverlapObjects(Vector3 position, float distance, int layerMask)
+        {
+            if (dimensionType == DimensionType.Dimension3D)
+                return Physics.OverlapSphereNonAlloc(position, distance, overlapColliders, layerMask);
+            return Physics2D.OverlapCircleNonAlloc(position, distance, overlapColliders2D, layerMask);
+        }
+
+        public GameObject GetOverlapObject(int index)
+        {
+            if (dimensionType == DimensionType.Dimension3D)
+                return overlapColliders[index].gameObject;
+            return overlapColliders2D[index].gameObject;
+        }
+
+        public bool FindTarget(GameObject target, float actDistance, int layerMask)
+        {
+            tempCount = OverlapObjects(CharacterTransform.position, actDistance, layerMask);
+            for (tempCounter = 0; tempCounter < tempCount; ++tempCounter)
+            {
+                tempGameObject = GetOverlapObject(tempCounter);
+                if (tempGameObject == target)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
