29d5c8a8ccd4e0a219f1ec0b68ad6d6400157b23
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-11-13 02:23:51 +0700

    Improve workflows

--- Scripts/TopDownPlayerCharacterController.cs
+++ Scripts/TopDownPlayerCharacterController.cs
@@ -7,10 +7,12 @@ namespace MultiplayerARPG
 {
     public sealed partial class TopDownPlayerCharacterController : PlayerCharacterController
     {
+        private bool cantSetDestination;
+
         protected override void Update()
         {
             pointClickSetTargetImmediately = true;
-            SelectedEntity = PlayerCharacterEntity.GetTargetEntity();
+            controllerMode = PlayerCharacterControllerMode.PointClick;
             base.Update();
         }
 
@@ -20,76 +22,91 @@ namespace MultiplayerARPG
             if (CurrentBuildingEntity != null)
                 return;
 
-            if (controllerMode != PlayerCharacterControllerMode.PointClick &&
-                controllerMode != PlayerCharacterControllerMode.Both)
-                return;
-
-            getMouseDown = Input.GetMouseButtonDown(0);
-            getMouseUp = Input.GetMouseButtonUp(0);
-            getMouse = Input.GetMouseButton(0);
-
             isPointerOverUI = CacheUISceneGameplay != null && CacheUISceneGameplay.IsPointerOverUIObject();
             if (isPointerOverUI)
                 return;
 
+            // Temp mouse input value
+            getMouseDown = Input.GetMouseButtonDown(0);
+            getMouseUp = Input.GetMouseButtonUp(0);
+            getMouse = Input.GetMouseButton(0);
+
             if (getMouseUp)
             {
                 // Clear target when player release mouse button
-                targetEntity = null;
+                ClearTarget(true);
                 return;
             }
 
             // Prepare temp variables
             Transform tempTransform;
             Vector3 tempVector3;
+            int tempCount;
 
-            if (getMouseDown)
+            // Clear target
+            if (!getMouse)
+                SelectedEntity = null;
+
+            tempCount = FindClickObjects(out tempVector3);
+            for (int tempCounter = 0; tempCounter < tempCount; ++tempCounter)
             {
-                // Clear target
-                targetEntity = null;
-                int tempCount = FindClickObjects(out tempVector3);
-                for (int tempCounter = 0; tempCounter < tempCount; ++tempCounter)
+                tempTransform = GetRaycastTransform(tempCounter);
+                targetPlayer = tempTransform.GetComponent<BasePlayerCharacterEntity>();
+                targetMonster = tempTransform.GetComponent<BaseMonsterCharacterEntity>();
+                targetNpc = tempTransform.GetComponent<NpcEntity>();
+                targetItemDrop = tempTransform.GetComponent<ItemDropEntity>();
+                targetHarvestable = tempTransform.GetComponent<HarvestableEntity>();
+                BuildingMaterial buildingMaterial = tempTransform.GetComponent<BuildingMaterial>();
+                targetPosition = GetRaycastPoint(tempCounter);
+                PlayerCharacterEntity.SetTargetEntity(null);
+                lastNpcObjectId = 0;
+                if (targetPlayer != null && !targetPlayer.GetCaches().IsHide)
                 {
-                    tempTransform = GetRaycastTransform(tempCounter);
-                    targetPlayer = tempTransform.GetComponent<BasePlayerCharacterEntity>();
-                    targetMonster = tempTransform.GetComponent<BaseMonsterCharacterEntity>();
-                    targetNpc = tempTransform.GetComponent<NpcEntity>();
-                    targetItemDrop = tempTransform.GetComponent<ItemDropEntity>();
-                    targetHarvestable = tempTransform.GetComponent<HarvestableEntity>();
-                    BuildingMaterial buildingMaterial = tempTransform.GetComponent<BuildingMaterial>();
-                    targetPosition = GetRaycastPoint(tempCounter);
-                    PlayerCharacterEntity.SetTargetEntity(null);
-                    lastNpcObjectId = 0;
-                    if (targetPlayer != null && !targetPlayer.IsDead())
-                    {
-                        SetTarget(targetPlayer);
-                        break;
-                    }
-                    else if (targetMonster != null && !targetMonster.IsDead())
-                    {
-                        SetTarget(targetMonster);
-                        break;
-                    }
-                    else if (targetNpc != null)
-                    {
-                        SetTarget(targetNpc);
-                        break;
-                    }
-                    else if (targetItemDrop != null)
-                    {
-                        SetTarget(targetItemDrop);
-                        break;
-                    }
-                    else if (targetHarvestable != null && !targetHarvestable.IsDead())
-                    {
-                        SetTarget(targetHarvestable);
-                        break;
-                    }
-                    else if (buildingMaterial != null && buildingMaterial.buildingEntity != null && !buildingMaterial.buildingEntity.IsDead())
-                    {
-                        SetTarget(buildingMaterial.buildingEntity);
-                        break;
-                    }
+                    if (!getMouse)
+                        SelectedEntity = targetPlayer;
+                    if (getMouseDown)
+                        SetTarget(targetPlayer, TargetActionType.Attack);
+                    break;
+                }
+                else if (targetMonster != null && !targetMonster.GetCaches().IsHide)
+                {
+                    if (!getMouse)
+                        SelectedEntity = targetMonster;
+                    if (getMouseDown)
+                        SetTarget(targetMonster, TargetActionType.Attack);
+                    break;
+                }
+                else if (targetNpc != null)
+                {
+                    if (!getMouse)
+                        SelectedEntity = targetNpc;
+                    if (getMouseDown)
+                        SetTarget(targetNpc, TargetActionType.Undefined);
+                    break;
+                }
+                else if (targetItemDrop != null)
+                {
+                    if (!getMouse)
+                        SelectedEntity = targetItemDrop;
+                    if (getMouseDown)
+                        SetTarget(targetItemDrop, TargetActionType.Undefined);
+                    break;
+                }
+                else if (targetHarvestable != null && !targetHarvestable.IsDead())
+                {
+                    if (!getMouse)
+                        SelectedEntity = targetHarvestable;
+                    if (getMouseDown)
+                        SetTarget(targetHarvestable, TargetActionType.Undefined);
+                    break;
+                }
+                else if (buildingMaterial != null && buildingMaterial.buildingEntity != null && !buildingMaterial.buildingEntity.IsDead())
+                {
+                    if (!getMouse)
+                        SelectedEntity = buildingMaterial.buildingEntity;
+                    if (getMouseDown)
+                        SetTarget(buildingMaterial.buildingEntity, TargetActionType.Undefined);
+                    break;
                 }
             }
 
@@ -99,56 +116,42 @@ namespace MultiplayerARPG
                 if (CacheUISceneGameplay != null && CacheUISceneGameplay.uiNpcDialog != null)
                     CacheUISceneGameplay.uiNpcDialog.Hide();
 
+                if (TargetEntity != null)
+                    cantSetDestination = true;
+
                 // Move to target
-                if (targetEntity != null)
-                {
-                    // Hide destination when target is object (not map ground)
-                    destination = null;
-                    PlayerCharacterEntity.SetTargetEntity(targetEntity);
-                }
-                else
+                if (!cantSetDestination)
                 {
                     // When moving, find target position which mouse click on
-                    int tempCount = FindClickObjects(out tempVector3);
-                    if (tempCount > 0)
+                    targetPosition = GetRaycastPoint(0);
+                    // When clicked on map (any non-collider position)
+                    // tempVector3 is come from FindClickObjects()
+                    // - Clear character target to make character stop doing actions
+                    // - Clear selected target to hide selected entity UIs
+                    // - Set target position to position where mouse clicked
+                    if (gameInstance.DimensionType == DimensionType.Dimension2D)
                     {
-                        targetPosition = GetRaycastPoint(0);
-                        // When clicked on map (any non-collider position)
-                        // tempVector3 is come from FindClickObjects()
-                        // - Clear character target to make character stop doing actions
-                        // - Clear selected target to hide selected entity UIs
-                        // - Set target position to position where mouse clicked
-                        if (gameInstance.DimensionType == DimensionType.Dimension2D)
-                        {
-                            PlayerCharacterEntity.SetTargetEntity(null);
-                            tempVector3.z = 0;
-                            targetPosition = tempVector3;
-                        }
+                        PlayerCharacterEntity.SetTargetEntity(null);
+                        tempVector3.z = 0;
+                        targetPosition = tempVector3;
                     }
                     destination = targetPosition;
                     PlayerCharacterEntity.PointClickMovement(targetPosition.Value);
                 }
             }
-        }
-
-        protected override void SetTarget(BaseGameEntity entity)
-        {
-            targetPosition = entity.CacheTransform.position;
-            targetEntity = entity;
-            PlayerCharacterEntity.SetTargetEntity(entity);
-            SelectedEntity = entity;
-        }
-
-        private void ClearTargetEntityAfterAttack()
-        {
-            if (targetEntity == null)
+            else
             {
-                // If target entity is not null it's means player still hold on mouse button
-                // because target entity will be cleared when mouse up event fire
-                ClearQueueUsingSkill();
-                PlayerCharacterEntity.SetTargetEntity(null);
-                PlayerCharacterEntity.StopMove();
+                if (TargetEntity == null)
+                    cantSetDestination = false;
             }
+        }
+
+        protected override void SetTarget(BaseGameEntity entity, TargetActionType targetActionType, bool checkControllerMode = true)
+        {
+            this.targetActionType = targetActionType;
+            destination = null;
+            TargetEntity = entity;
+            PlayerCharacterEntity.SetTargetEntity(entity);
         }
     }
 }
